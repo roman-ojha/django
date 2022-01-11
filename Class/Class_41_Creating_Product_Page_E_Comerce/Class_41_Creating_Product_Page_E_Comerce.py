d2309e1a72afbee7c708b87beb71bffeb77f8636
Roman Ojha <razzroman98@gmail.com>
2022-01-11 11:35:15 +0545

    creating product page class

---
+++ Class/Class_41_Creating_Product_Page_E_Comerce/Class_41_Creating_Product_Page_E_Comerce.py
@@ -0,0 +1,5 @@
+"""
+-> in urls.py of show if we want to go the productview of all product then we have to add id in that url
+
+=> now we will create a prodView.html page  and about.html
+"""
---
+++ Class/Class_41_Creating_Product_Page_E_Comerce/Ecommerce/shop/views.py
@@ -0,0 +1,47 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Product
+from math import ceil
+
+# Create your views here.
+
+
+def index(request):
+    allProds = []
+    catprods = Product.objects.values('category', 'id')
+    cats = {item['category'] for item in catprods}
+    for cat in cats:
+        prod = Product.objects.filter(category=cat)
+        n = len(prod)
+        nSlides = n//4+ceil((n/4)-(n//4))
+        allProds.append([prod, range(1, nSlides), nSlides])
+    params = {'allProds': allProds}
+    return render(request, 'shop/index.html', params)
+
+
+def about(request):
+    return render(request, 'shop/about.html')
+
+
+def contact(request):
+    return render(request, 'shop/contact.html')
+
+
+def tracker(request):
+    return render(request, 'shop/tracker.html')
+
+
+def search(request):
+    return render(request, 'shop/search.html')
+
+
+def productview(request, myid):
+    # now we can get the id here that use had passed through url
+    product = Product.objects.filter(id=myid)
+    print(product)
+    return render(request, 'shop/prodView.html', {"product": product[0]})
+    # now we will send the requested id product to page
+
+
+def checkout(request):
+    return render(request, 'shop/checkout.html')
