1bd940213fbd6eb022e523c1a6ff89fe41eef6bc
Roman-Ojha <razzroman98@gmail.com>
2022-07-18 21:58:57 +0545

    fix: deleted mongoDb integration as repo to add as submodule

--- Tutorial/Tut_02_MongoDB_Integration/Tut_02_MongoDB_Integration.py
+++
@@ -1,6 +0,0 @@
-"""
--> pip install -U autopep8 (package for auto formatting)
--> pip install django (install django package)
--> pip install djongo (mongodb for django)
--> pip install dnspython
-"""
