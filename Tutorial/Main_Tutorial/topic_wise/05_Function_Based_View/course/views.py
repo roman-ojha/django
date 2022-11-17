61ec30324447520c10a0d98b822172a9533405c1
Roman-Ojha <razzroman98@gmail.com>
2022-11-17 21:59:33 +0545

    docs: learnd Function Based View

--- Tutorial/Main_Tutorial/topic_wise/05_Function_Based_View/course/views.py
+++ Tutorial/Main_Tutorial/topic_wise/05_Function_Based_View/course/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
-# Create your views here.
+# import HttpResponse first
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Home Page")
+
+
+def learn_django(request):
+    # view function take 'request' object as params
+
+    return HttpResponse('<h1> Hello Django </h1>')
+
+
+def learn_php(request):
+    return HttpResponse('<h1> Hello PHP </h1>')
+
+
+def learn_math(request):
+    a = 10 + 10
+    return HttpResponse(a)
