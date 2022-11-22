3c77bf60782d3023abae8c80531a03dc6a95e26a
Roman-Ojha <razzroman98@gmail.com>
2022-11-22 23:01:25 +0545

    docs: learnd django if else tag & float filter

--- Tutorial/Main_Tutorial/topic_wise/12_Django_Template_Language/course/views.py
+++ Tutorial/Main_Tutorial/topic_wise/12_Django_Template_Language/course/views.py
@@ -6,7 +6,8 @@ from datetime import datetime
 def learn_django(request):
     # passing date for date & time filter
     date = datetime.now()
-    data = {'cname': 'django', 'duration': "4 Months", "seats": 18, 'date': date}
+    data = {'cname': 'django', 'duration': "4 Months",
+            "seats": 18, 'date': date, 'price': 123.432130}
     return render(request, 'course/index.html', context=data)
 
 
