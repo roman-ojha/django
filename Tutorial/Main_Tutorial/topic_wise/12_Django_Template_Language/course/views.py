2cfeaca33233bdf346681cf051c83257ac8a3277
Roman-Ojha <razzroman98@gmail.com>
2022-11-23 21:13:11 +0545

    docs: for loop & dot lookup & predefined forloop variables

--- Tutorial/Main_Tutorial/topic_wise/12_Django_Template_Language/course/views.py
+++ Tutorial/Main_Tutorial/topic_wise/12_Django_Template_Language/course/views.py
@@ -6,8 +6,14 @@ from datetime import datetime
 def learn_django(request):
     # passing date for date & time filter
     date = datetime.now()
+    students = {'names': ['roman', 'razz', 'jack', 'harry', 'tom', 'tony']}
+    stu = {'stu1': {'name': 'razz', 'roll': 31},
+           'stu2': {'name': 'roman', 'roll': 25},
+           'stu3': {'name': 'suman', 'roll': 43},
+           'stu4': {'name': 'manish', 'roll': 17}
+           }
     data = {'cname': 'django', 'duration': "4 Months",
-            "seats": 18, 'date': date, 'price': 123.432130}
+            "seats": 18, 'date': date, 'price': 123.432130, 'students': students, 'stu': stu}
     return render(request, 'course/index.html', context=data)
 
 
