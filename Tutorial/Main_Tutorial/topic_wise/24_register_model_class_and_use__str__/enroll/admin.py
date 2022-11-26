6a601a1f6e6a00dcbfed281637184958932cad95
Roman-Ojha <razzroman98@gmail.com>
2022-11-26 20:38:37 +0545

    feat: created, registered & use __str__ method for model

--- Tutorial/Main_Tutorial/topic_wise/24_register_model_class_and_use__str__/enroll/admin.py
+++ Tutorial/Main_Tutorial/topic_wise/24_register_model_class_and_use__str__/enroll/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+# first we will import the model class
+from .models import Student
+
 # Register your models here.
+admin.site.register(Student)
--- Tutorial/Main_Tutorial/topic_wise/24_register_model_class_and_use__str__/enroll/models.py
+++ Tutorial/Main_Tutorial/topic_wise/24_register_model_class_and_use__str__/enroll/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Student(models.Model):
+    stuid = models.IntegerField()
+    stuname = models.CharField(max_length=70)
+    stuemail = models.EmailField(max_length=70)
+    stupass = models.CharField(max_length=70)
+
+    def __str__(self) -> str:
+        # we will return the filed that we want to see on django admin interface
+
+        # return self.stuname
+        return f"{self.stuid} {self.stuname}"
+        # EX: 102 harry
