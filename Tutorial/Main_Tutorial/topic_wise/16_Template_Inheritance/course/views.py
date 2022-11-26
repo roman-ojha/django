183be19dacc0459091710706f9aaebc73d13d66f
Roman-Ojha <razzroman98@gmail.com>
2022-11-26 08:19:31 +0545

    feat: added urls + template + views for template inheritance

--- Tutorial/Main_Tutorial/topic_wise/16_Template_Inheritance/course/views.py
+++ Tutorial/Main_Tutorial/topic_wise/16_Template_Inheritance/course/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def learn_django(request):
+    print("jfdksfjdlsakfjdslakfjdsalkfjdslakfdjsalkfdjsalfkdsjla")
+    return render(request, 'course/info.html')
