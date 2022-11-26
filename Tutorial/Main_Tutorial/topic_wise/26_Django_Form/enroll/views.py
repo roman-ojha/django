f65df8140a0c915a2320bbb516fa0354dd0164eb
Roman-Ojha <razzroman98@gmail.com>
2022-11-26 23:04:41 +0545

    feat: create from class & showing form inside template

--- Tutorial/Main_Tutorial/topic_wise/26_Django_Form/enroll/views.py
+++ Tutorial/Main_Tutorial/topic_wise/26_Django_Form/enroll/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+# importing forms class
+from .forms import StudentRegistration
+
+
+def showFormData(request):
+    # creating object of form data to pass into template as object
+    form = StudentRegistration()
+    return render(request, 'enroll/registration.html', {'form': form})
