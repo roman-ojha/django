8fff3751e674f5176ae45f31655055e2b11722de
Roman-Ojha <razzroman98@gmail.com>
2022-11-26 14:59:45 +0545

    feat: created 'navbar' & 'topcourse' template and use include tag to include template

--- Tutorial/Main_Tutorial/topic_wise/19_include_template_within_template/core/views.py
+++ Tutorial/Main_Tutorial/topic_wise/19_include_template_within_template/core/views.py
@@ -4,7 +4,8 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'core/home.html', {'about_url': '/about'})
+    # NOTE: the context that we pass from here that can be able to access from 'core/home.html' but if that template include another template then that template can also access the context passed from here
+    return render(request, 'core/home.html', {'about_url': '/about', 'top_course_template': ['Dart', 'Vue', 'React', 'Typescript']})
 
 
 def about(request):
