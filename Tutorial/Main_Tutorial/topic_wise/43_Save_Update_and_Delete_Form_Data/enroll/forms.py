02bc07d2f142cc5e0c807c820e100757acd714a1
Roman-Ojha <razzroman98@gmail.com>
2022-11-27 17:25:37 +0545

    feat: saved, updated & delete user into and from database

--- Tutorial/Main_Tutorial/topic_wise/43_Save_Update_and_Delete_Form_Data/enroll/forms.py
+++ Tutorial/Main_Tutorial/topic_wise/43_Save_Update_and_Delete_Form_Data/enroll/forms.py
@@ -6,10 +6,6 @@ from django.core import validators
 class StudentRegistration(forms.Form):
     name = forms.CharField(error_messages={'required': 'Enter your name'})
     email = forms.EmailField(
-        error_messages={'required': 'Enter your Email'}, min_length=5, max_length=50)
+        error_messages={'required': 'Enter your Email'})
     password = forms.CharField(widget=forms.PasswordInput(), error_messages={
-                               'required': 'Enter your Password'}, min_length=5, max_length=50)
-
-    # here we are giving the class name of the error if it occur
-    error_css_class = 'error'
-    required_css_class = 'required'
+                               'required': 'Enter your Password'})
