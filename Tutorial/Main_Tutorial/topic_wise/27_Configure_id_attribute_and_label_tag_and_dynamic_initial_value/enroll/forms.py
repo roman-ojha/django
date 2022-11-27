9277a1c9e5fdbb905e261891bd63cdfcab2b81a4
Roman-Ojha <razzroman98@gmail.com>
2022-11-27 08:56:04 +0545

    feat: configured id attrubute, label tag & added dynamic value inside form

--- Tutorial/Main_Tutorial/topic_wise/27_Configure_id_attribute_and_label_tag_and_dynamic_initial_value/enroll/forms.py
+++ Tutorial/Main_Tutorial/topic_wise/27_Configure_id_attribute_and_label_tag_and_dynamic_initial_value/enroll/forms.py
@@ -5,3 +5,6 @@ from django import forms
 class StudentRegistration(forms.Form):
     name = forms.CharField()
     email = forms.EmailField()
+    first_name = forms.CharField()
+    # this will generate label:
+    # <label for="first_name">First name </label>
--- Tutorial/Main_Tutorial/topic_wise/27_Configure_id_attribute_and_label_tag_and_dynamic_initial_value/enroll/views.py
+++ Tutorial/Main_Tutorial/topic_wise/27_Configure_id_attribute_and_label_tag_and_dynamic_initial_value/enroll/views.py
@@ -3,5 +3,33 @@ from .forms import StudentRegistration
 
 
 def showFormData(request):
-    form = StudentRegistration()
+    # auto_id:
+    form = StudentRegistration(auto_id='some_%s')
+    # Output: <input type="text" name="name" required id="some_name">
+
+    form = StudentRegistration(auto_id=True)
+    # Output: <input type="text" name="name" required id="name">
+    form = StudentRegistration(auto_id='anyString')
+    # Output: <input type="text" name="name" required id="name">
+
+    form = StudentRegistration(auto_id=False)
+    # will remove label tag and:
+    # Output: <input type="text" name="name" required>
+
+    # label_suffix:
+    form = StudentRegistration(auto_id=True, label_suffix='')
+    # Output: <label for="name">Name</label>
+
+    form = StudentRegistration(auto_id=True, label_suffix='/roman')
+    # Output: <label for="name">Name/roman</label>
+
+    # initial:
+    form = StudentRegistration(
+        auto_id=True, label_suffix=' ', initial={'name': 'roman'})
+    # Output: <input type="text" name="name" value="roman" required id="name">
+    form = StudentRegistration(
+        auto_id=True, label_suffix=' ', initial={'name': 'roman', 'email': 'roman@gmail.com'})
+
+    form = StudentRegistration(
+        auto_id=True, label_suffix=' ', initial={'name': 'roman'})
     return render(request, 'enroll/registration.html', {'form': form})
