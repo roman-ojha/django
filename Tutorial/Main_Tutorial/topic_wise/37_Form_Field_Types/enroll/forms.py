0b4143c7517b437bd40828567d61bcf561ca1e17
Roman-Ojha <razzroman98@gmail.com>
2022-11-27 13:22:33 +0545

    docs: learned django Form Field Types

--- Tutorial/Main_Tutorial/topic_wise/37_Form_Field_Types/enroll/forms.py
+++ Tutorial/Main_Tutorial/topic_wise/37_Form_Field_Types/enroll/forms.py
@@ -2,6 +2,26 @@ from django import forms
 
 
 class StudentRegistration(forms.Form):
-    name = forms.CharField()
-    email = forms.EmailField()
-    password = forms.CharField(widget=forms.PasswordInput())
+    # adding 'min_length' & 'max_length' validator
+    name = forms.CharField(min_length=5, max_length=20,
+                           strip=True, empty_value='', error_messages={'required': 'Name field is required', 'min_length': 'char length should be greater then 4'})
+    # strip=True (white space will get removed)
+    # strip=False (white space will not get removed)
+    # empty_value (is the default value of use didn't get passed, now required will not throw an error)
+    # error_message (change default error message)
+
+    roll = forms.IntegerField(min_value=5, max_value=100)
+    price = forms.DecimalField(
+        min_value=5, max_value=40, max_digits=4, decimal_places=1)
+    rate = forms.FloatField(min_value=5, max_value=40)
+    comment = forms.SlugField()
+    email = forms.EmailField(min_length=5, max_length=50)
+    website = forms.URLField(min_length=5, max_length=100)
+    password = forms.CharField(
+        min_length=5, max_length=50, widget=forms.PasswordInput())
+    description = forms.CharField(widget=forms.Textarea)
+    feedback = forms.CharField(
+        min_length=5, max_length=50, widget=forms.TextInput())
+    notes = forms.CharField(widget=forms.Textarea(
+        attrs={'rows': 3, 'cols': 50}))
+    agree = forms.BooleanField(label="Agreed terms and condition?")
