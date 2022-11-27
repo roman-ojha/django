b61e52a7710e21f7cfd1ac5d9859e1f6e1a3df49
Roman-Ojha <razzroman98@gmail.com>
2022-11-27 21:23:45 +0545

    feat: create dynamic url to get extra data from url

--- Tutorial/Main_Tutorial/topic_wise/45_Dynamic_URL/config/urls.py
+++ Tutorial/Main_Tutorial/topic_wise/45_Dynamic_URL/config/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('register', include('enroll.urls')),
+    path('enroll/', include('enroll.urls')),
 ]
--- Tutorial/Main_Tutorial/topic_wise/45_Dynamic_URL/enroll/views.py
+++ Tutorial/Main_Tutorial/topic_wise/45_Dynamic_URL/enroll/views.py
@@ -1,36 +1,36 @@
 from django.shortcuts import render
-from .forms import UserRegistration
-from .models import User
 
 
-def register(request):
-    if request.method == "POST":
-        # to update the data we will first get the data
-        # so updating the data that have id = 2
-        pi = User.objects.get(pk=2)
+def home(request):
+    return render(request, 'enroll/home.html')
 
-        # by passing the instance into the ModelForm it will bind those value that user enter with the model and now we can update that data
-        form = UserRegistration(request.POST, instance=pi)
 
-        # on update
-        operation = 'update'
+def show_details(request, id=1):
+    # getting 'id' from dynamic urls value
+    # we can use **kwargs as well to get the dynamic urls values
+    # we can also define the default value if the value didn't came from url
+    print(id)
+    return render(request, 'enroll/show.html', {'id': id, "name": "Null"})
 
-        # on save
-        # operation = 'save'
 
-        if form.is_valid():
-            if operation is 'save':
-                # saving the data
-                print(form.cleaned_data)
-                name = form.cleaned_data['name']
-                email = form.cleaned_data['email']
-                password = form.cleaned_data['password']
+def show_details_int(request, id):
+    # using id as int
+    if id == 1:
+        return render(request, 'enroll/show.html', {'id': id, 'name': "Roman"})
+    if id == 2:
+        return render(request, 'enroll/show.html', {'id': id, 'name': "Razz"})
+    return render(request, 'enroll/show.html', {'id': id, "name": "Null"})
 
-                reg = User(name=name, email=email, password=password)
-                reg.save()
-            else:
-                # updating the data
-                form.save()
-    else:
-        form = UserRegistration()
-    return render(request, 'enroll/registration.html', {'form': form})
+
+def show_sub_details_int(request, id, sub_id):
+    # using id as int
+    if id == 1 and sub_id == 2:
+        return render(request, 'enroll/show.html', {'id': id, 'name': "Roman"})
+    if id == 2 and sub_id == 1:
+        return render(request, 'enroll/show.html', {'id': id, 'name': "Razz"})
+    return render(request, 'enroll/show.html', {'id': id, "name": "Null"})
+
+
+def kwargs_view(request, id, name):
+    # getting 'id' from kwargs
+    return render(request, 'enroll/show.html', {'id': id, "name": name})
