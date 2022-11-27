6aa4273a1b1bef95c2f45fa1a20b5de6533d8e50
Roman-Ojha <razzroman98@gmail.com>
2022-11-27 17:00:38 +0545

    feat: styled django form errors

--- Tutorial/Main_Tutorial/topic_wise/42_Style_Form_Errors/enroll/forms.py
+++ Tutorial/Main_Tutorial/topic_wise/42_Style_Form_Errors/enroll/forms.py
@@ -1,18 +1,15 @@
 from django import forms
 
-# first import validators
 from django.core import validators
-
-# creating custom validator function
-
-
-def start_with_r(value):
-    if value[0] != 'r':
-        raise forms.ValidationError("Name should start with 'r' char")
 
 
 class StudentRegistration(forms.Form):
-    # now we have to pass the list of validators into formField
-    name = forms.CharField(validators=[validators.MaxLengthValidator(
-        10), validators.MinLengthValidator(5), start_with_r])
-    email = forms.EmailField()
+    name = forms.CharField(error_messages={'required': 'Enter your name'})
+    email = forms.EmailField(
+        error_messages={'required': 'Enter your Email'}, min_length=5, max_length=50)
+    password = forms.CharField(widget=forms.PasswordInput(), error_messages={
+                               'required': 'Enter your Password'}, min_length=5, max_length=50)
+
+    # here we are giving the class name of the error if it occur
+    error_css_class = 'error'
+    required_css_class = 'required'
