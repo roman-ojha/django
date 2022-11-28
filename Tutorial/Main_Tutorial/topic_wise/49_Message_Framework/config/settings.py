c479cbb8d683c45d0df1235e2345861c9f4c1ff2
Roman-Ojha <razzroman98@gmail.com>
2022-11-28 10:47:14 +0545

    feat: used message framework methods & changed message tags name

--- Tutorial/Main_Tutorial/topic_wise/49_Message_Framework/config/settings.py
+++ Tutorial/Main_Tutorial/topic_wise/49_Message_Framework/config/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from django.contrib.messages import constants as msg
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -107,3 +108,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Renaming Message tag
+MESSAGE_TAGS = {
+    msg.ERROR: 'danger'
+}
--- Tutorial/Main_Tutorial/topic_wise/49_Message_Framework/enroll/views.py
+++ Tutorial/Main_Tutorial/topic_wise/49_Message_Framework/enroll/views.py
@@ -7,7 +7,7 @@ def register(request):
     if request.method == "POST":
         form = StudentRegistration(request.POST)
         if form.is_valid():
-            form.save()
+            # form.save()
             # after we register the user and save it into database we will create the message saying register successfully
             # and then we will show that message to the template
 
@@ -18,6 +18,37 @@ def register(request):
 
             # different way to send the message
             messages.info(request, "Now you can log in !!!")
+
+            # getting level
+            print(messages.get_level(request))
+            # return: 20
+            # by default we can't see the level below 20 or from below INFO
+            # until we set the debug level
+
+            # can't get level for debug before we set the level
+            messages.debug(request, "We are in Debug environment before set")
+
+            # setting level
+            messages.set_level(request, messages.DEBUG)
+
+            # now we can see this debug message into the template
+            messages.debug(request, "We are in Debug environment after set")
+
+            # now getting debug
+            print(messages.get_level(request))
+            # return: 10
+
+            # sending other messages
+            messages.warning(request, "This is warning")
+
+            # we had change the message tag name of error to danger in 'settings.py'
+            # EX: MESSAGE_TAGS = { msg.ERROR: 'danger'}
+            messages.error(request, "This is error")
+
+            # if we want to get the message inside the python file rather then in template then we can use this
+            storage = messages.get_messages(request)
+            for message in storage:
+                print(message)
 
     else:
         form = StudentRegistration()
