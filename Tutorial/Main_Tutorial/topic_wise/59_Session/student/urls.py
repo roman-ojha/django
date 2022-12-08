d47095946b7a0680e2ee85376f97e8852db55109
Roman-Ojha <razzroman98@gmail.com>
2022-12-08 09:46:04 +0545

    feat: used session method like: to se exipiry age, set & del & check test cookie

--- Tutorial/Main_Tutorial/topic_wise/59_Session/student/urls.py
+++ Tutorial/Main_Tutorial/topic_wise/59_Session/student/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('del/', views.deleteSession),
     path('methods/', views.methods),
     # path('get/signed/', views.getSignedCookies),
+    path('test/set', views.setTestCookie),
+    path('test/check', views.checkTestCookie),
+    path('test/del', views.deleteTestCookie),
 ]
