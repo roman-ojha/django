4af2e8eff8aae21fbd9ef4b24808b15fce0f8fcd
Roman-Ojha <razzroman98@gmail.com>
2022-12-08 12:59:51 +0545

    feat: used template fragment cache

--- Tutorial/Main_Tutorial/topic_wise/63_Template_Fragment_Cache/enroll/urls.py
+++ Tutorial/Main_Tutorial/topic_wise/63_Template_Fragment_Cache/enroll/urls.py
@@ -4,8 +4,4 @@ from django.views.decorators.cache import cache_page
 
 urlpatterns = [
     path('course/', views.home),
-
-    # cache using url
-    # NOTE: cache will be different for every dynamic url value if it exist
-    path('contact/', cache_page(10)(views.contact)),
 ]
--- Tutorial/Main_Tutorial/topic_wise/63_Template_Fragment_Cache/enroll/views.py
+++ Tutorial/Main_Tutorial/topic_wise/63_Template_Fragment_Cache/enroll/views.py
@@ -4,12 +4,5 @@ from django.shortcuts import render
 from django.views.decorators.cache import cache_page
 
 
-@cache_page(20)
-# caching home page
-# @cache_page(<timeout>)
 def home(request):
     return render(request, 'enroll/course.html')
-
-
-def contact(request):
-    return render(request, 'enroll/contact.html')
