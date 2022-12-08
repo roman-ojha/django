dc5e76eab025cec20ce2609b999df3f156af48c7
Roman-Ojha <razzroman98@gmail.com>
2022-12-08 20:10:43 +0545

    feat: created custom signals

--- Tutorial/Main_Tutorial/topic_wise/66_Custom_Signals/blog/views.py
+++ Tutorial/Main_Tutorial/topic_wise/66_Custom_Signals/blog/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from blog import signals
 
-# Create your views here.
+
+def home(request):
+    # sending signal
+    signals.notification.send(
+        sender=request.user, request=request, user=['Roman', 'Razz'])
+    # we are providing the argument that we define while creating signals
+    return HttpResponse('From home page')
