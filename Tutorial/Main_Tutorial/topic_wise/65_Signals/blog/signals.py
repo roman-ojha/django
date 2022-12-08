27ad7b459ab5c2d33224348d2a853eff7918de76
Roman-Ojha <razzroman98@gmail.com>
2022-12-08 18:22:31 +0545

    feat: used User Model related signals

--- Tutorial/Main_Tutorial/topic_wise/65_Signals/blog/signals.py
+++ Tutorial/Main_Tutorial/topic_wise/65_Signals/blog/signals.py
@@ -1,5 +1,6 @@
 # importing signals
 from django.contrib.auth.signals import user_logged_in, user_logged_out, user_login_failed
+from django.db.models.signals import pre_init, pre_save, pre_delete, post_init, post_save, post_delete
 # importing User model it will become the sender
 from django.contrib.auth.models import User
 from django.dispatch import receiver
@@ -45,8 +46,91 @@ def login_fail(sender, credentials, request, **kwargs):
     # run this function when user try to login and get failed to login
     # we will get extra 'credentials' data which contain the login credentials that user used to login
     print("------------------------------------")
-    print("Login Failed Signal...... Run Out..")
+    print("Login Failed Signal......")
     print("Sender: ", sender)
     print("Request: ", request)
     print('Credentials: ', credentials)
     print(f'Kwargs {kwargs}')
+# user_login_failed.connect(login_fail)
+
+
+@receiver(pre_save, sender=User)
+def at_beginning_save(sender, instance, **kwargs):
+    # function that will get called before the new instance of mode being saved
+    print("------------------------------------")
+    print("Pre Save Signal....")
+    print("Sender: ", sender)
+    print("Instance: ", instance)
+    print(f'Kwargs {kwargs}')
+# pre_save.connect(at_beginning_save,sender=User)
+
+
+@receiver(post_save, sender=User)
+def at_ending_save(sender, instance, created, **kwargs):
+    # function that will get called after the new instance of mode get saved
+    if created:
+        # if record is being created
+        print("------------------------------------")
+        print("Post Save Signal....")
+        print("New Record")
+        print("Sender: ", sender)
+        print("Instance: ", instance)
+        print("Created: ", created)
+        print(f'Kwargs {kwargs}')
+    else:
+        # if record is being update
+        # we can renew the cache if user Update
+        print("------------------------------------")
+        print("Post Save Signal....")
+        print("Update")
+        print("Sender: ", sender)
+        print("Instance: ", instance)
+        print("Created: ", created)
+        print(f'Kwargs {kwargs}')
+# post_save.connect(at_ending_save, sender=User)
+
+
+@receiver(pre_delete, sender=User)
+def at_beginning_delete(sender, instance, **kwargs):
+    # function that will get called before the model is being delete
+    print("------------------------------------")
+    print("Pre Delete Signal....")
+    print("Sender: ", sender)
+    print("Instance: ", instance)
+    print(f'Kwargs {kwargs}')
+# pre_delete.connect(at_beginning_delete,sender=User)
+
+
+@receiver(pre_delete, sender=User)
+def at_ending_delete(sender, instance, **kwargs):
+    # function that will get called after the model is get deleted
+    print("------------------------------------")
+    print("Post Delete Signal....")
+    print("Sender: ", sender)
+    print("Instance: ", instance)
+    print(f'Kwargs {kwargs}')
+# post_delete.connect(at_ending_delete, sender=User)
+
+
+@receiver(pre_init, sender=User)
+def at_beginning_init(sender, *args, **kwargs):
+    # function that will get called whenever the new django model is being instantiate
+    print("------------------------------------")
+    print("Pre Init Signal....")
+    print("Sender: ", sender)
+    print(f'Args {args}')
+    print(f'Kwargs {kwargs}')
+# pre_init.connect(at_beginning_init, sender=User)
+
+
+@receiver(post_init, sender=User)
+def at_ending_init(sender, *args, **kwargs):
+    # function that will get called whenever the new django model is being instantiate
+    print("------------------------------------")
+    print("Post Init Signal....")
+    print("Sender: ", sender)
+    print(f'Args {args}')
+    print(f'Kwargs {kwargs}')
+
+
+post_init.connect(at_ending_init, sender=User)
