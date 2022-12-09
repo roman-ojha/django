a94c1915bd21ffbb1bb7f38ccca3b9e359041f9c
Roman-Ojha <razzroman98@gmail.com>
2022-12-09 18:56:04 +0545

    feat: used get, first, last, latest, exist, create, update, bulk_create QuerySet methods

--- Tutorial/Main_Tutorial/topic_wise/69_QuerySet_API_Methods_do_not_returns_new_QS/school/views.py
+++ Tutorial/Main_Tutorial/topic_wise/69_QuerySet_API_Methods_do_not_returns_new_QS/school/views.py
@@ -1,97 +1,76 @@
 from django.shortcuts import render
-from .models import Student, Teacher
-from django.db.models import Q
-
-# Create your views here.
+from .models import Student
 
 
 def home(request):
-    # Getting all the data from table
-    students = Student.objects.all()
-    print(students)
-    # if you want to see the SQL query that this method is generating then you can use this:
-    print("SQL Query: ", students.query)
+    # get:
+    # get single data base on primary key
+    # also every row need to be uniquely identify with the from which we are trying to query
+    # also it need to match the data that we are trying to query
+    student = Student.objects.get(pk=1)
+    # we know id is also unique
+    student = Student.objects.get(id=2)
+    # we make roll unique as well
+    student = Student.objects.get(roll=25)
 
-    # Filter method to return by passing parameters:
-    students = Student.objects.filter(marks=300)
-    # getting all the student whose marks is 300
+    # first:
+    # get the first match data
+    student = Student.objects.first()
+    student = Student.objects.order_by('name').first()
+    student = Student.objects.order_by('-name').first()
+    student = Student.objects.filter(roll=25).first()
 
-    # Exclude:
-    students = Student.objects.exclude(marks=300)
-    # getting all the student whose marks is not 300
+    # last:
+    student = Student.objects.last()
+    student = Student.objects.order_by('name').last()
+    student = Student.objects.order_by('-name').last()
+    student = Student.objects.filter(roll=25).last()
 
-    # OrderBy:
-    # getting all the student order by roll
-    # Ascending order:
-    students = Student.objects.order_by('roll')
-    # Descending order:
-    students = Student.objects.order_by('-roll')
-    # Random order:
-    students = Student.objects.order_by('?')
-    # NOTE: it order based on unicode
-    # Order Ex: A, B, ....., Z, a, b, ...., z
-    # Reverse Order:
-    students = Student.objects.order_by('id').reverse()
-    # Limit: First 2
-    students = Student.objects.order_by('id').reverse()[:2]
+    # latest:
+    student = Student.objects.latest('pass_date')
 
-    # Values:
-    students = Student.objects.values()
-    # Getting all the students from table in dictionary list
-    students = Student.objects.values('name', 'city')
-    # Getting all the students name & city from table in dictionary list
+    # earliest
+    student = Student.objects.earliest('pass_date')
 
-    # Values_List:
-    students = Student.objects.values_list()
-    students = Student.objects.values_list('name', 'city')
-    # it return Tuple
-    students = Student.objects.values_list('name', 'city', named=True)
-    # return named tuple
-    # <QuerySet [('roman', 'kathmandu'), ('razz', 'kathmandu'), ('Jack', 'Mumbai')]>
+    # exists:
+    # check does data or not
+    student = Student.objects.all()
+    print(student.exists())
+    print(student.filter(pk=Student.objects.get(pk=1).pk).exists())
 
-    # Using: if you use multiple database and want to specify from which database you want to query in that case we can use 'using'
-    # the name that we specify on the 'setting.py' file is the name of database that we will specify here
-    """ 
-        DATABASES = {
-            'default': {
-                'ENGINE': 'django.db.backends.sqlite3',
-                'NAME': BASE_DIR / 'db.sqlite3',
-            }
-        }
-    """
-    students = Student.objects.using('default')
-    students = Student.objects.using('default').all()
+    # Create:
+    # to create|insert data
+    # student = Student.objects.create(
+    #     name='Tony', roll=33, city="Kathmandu", marks=321, pass_date='2020-5-4')
 
-    # Dates:
-    students = Student.objects.dates('pass_date', 'month')
-    print(students)
+    # get_or_create:
+    # get if exist if not then create
+    student, created = Student.objects.get_or_create(
+        name='Tony', roll=33, city="Kathmandu", marks=321, pass_date='2020-5-4')
 
-    # Union:
-    # using 'Student' & 'Teacher' models to perform union
-    qs1 = Student.objects.values_list('id', 'name', named=True)
-    qs2 = Teacher.objects.values_list('id', 'name', named=True)
-    students = qs2.union(qs1)
-    # Getting both 'Student' & 'Teacher' data and union it to get both data into one
-    # also it wil remove the duplicate data
-    # SELECT "school_teacher"."id", "school_teacher"."name" FROM "school_teacher" UNION SELECT "school_student"."id", "school_student"."name" FROM "school_student"
-    students = qs2.union(qs1, all=True)
-    # getting duplicate data as well
+    # update:
+    # to update the data
+    # NOTE: update method only work on QuerySet not only object
+    # update single data
+    student = Student.objects.filter(id=2).update(name="Razz Roman", marks=421)
+    # update multiple data
+    student = Student.objects.filter(marks=421).update(name="Razz", marks=500)
 
-    # Intersection
-    students = qs2.intersection(qs1)
-    # return similar data on both Table
+    # update or create
+    # update if exist if not create
+    # student, created = Student.objects.update_or_create(
+    #     id=4, name='Tony', defaults={'name': 'tony', 'roll': 30, 'city': "Kathmandu", 'marks': 321, 'pass_date': '2020-5-4'})
+    # if id=4 and name = 'Tony' exist then update 'defaults'
 
-    # Difference
-    students = qs2.difference(qs1)
-    students = qs1.difference(qs2)
+    # bulk_create:
+    # create multiple data at once
+    objs = [
+        Student(
+            name='Strange', roll=21, city="Kathmandu", marks=321, pass_date='2020-5-4'),
+        Student(
+            name='Superman', roll=22, city="Pokhara", marks=123, pass_date='2020-4-4'),
+    ]
+    # students = Student.objects.bulk_create(objs=objs)
 
-    # AND:
-    students = Student.objects.filter(id=2) & Student.objects.filter(roll=30)
-    students = Student.objects.filter(id=2, roll=30)
-    students = Student.objects.filter(Q(id=2) & Q(roll=30))
-    # OR:
-    students = Student.objects.filter(id=2) | Student.objects.filter(marks=300)
-    students = Student.objects.filter(Q(id=2) | Q(marks=300))
-    # SELECT "school_student"."id", "school_student"."name", "school_student"."roll", "school_student"."city", "school_student"."marks", "school_student"."pass_date" FROM "school_student" WHERE ("school_student"."id" = 2 OR "school_student"."marks" = 300)
-
-    return render(request, 'school/home.html', {'students': students})
+    # bulk_update:
+    return render(request, 'school/home.html', {'student': student})
