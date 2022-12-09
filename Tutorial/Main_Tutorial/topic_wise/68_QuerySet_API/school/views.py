a8bb26b8183dbe15a88763ea3e396bc1e3ee4ee4
Roman-Ojha <razzroman98@gmail.com>
2022-12-09 17:17:52 +0545

    feat: used orderby, reverse, values, values_list, date QuerySet methods

--- Tutorial/Main_Tutorial/topic_wise/68_QuerySet_API/school/views.py
+++ Tutorial/Main_Tutorial/topic_wise/68_QuerySet_API/school/views.py
@@ -11,13 +11,58 @@ def home(request):
     # if you want to see the SQL query that this method is generating then you can use this:
     print("SQL Query: ", students.query)
 
-    # Filter method to return by passing parameters
+    # Filter method to return by passing parameters:
     students = Student.objects.filter(marks=300)
     # getting all the student whose marks is 300
 
-    # Exclude
+    # Exclude:
     students = Student.objects.exclude(marks=300)
     # getting all the student whose marks is not 300
 
-    # OrderBy
+    # OrderBy:
+    # getting all the student order by roll
+    # Ascending order:
+    students = Student.objects.order_by('roll')
+    # Descending order:
+    students = Student.objects.order_by('-roll')
+    # Random order:
+    students = Student.objects.order_by('?')
+    # NOTE: it order based on unicode
+    # Order Ex: A, B, ....., Z, a, b, ...., z
+    # Reverse Order:
+    students = Student.objects.order_by('id').reverse()
+    # Limit: First 2
+    students = Student.objects.order_by('id').reverse()[:2]
+
+    # Values:
+    students = Student.objects.values()
+    # Getting all the students from table in dictionary list
+    students = Student.objects.values('name', 'city')
+    # Getting all the students name & city from table in dictionary list
+
+    # Values_List:
+    students = Student.objects.values_list()
+    students = Student.objects.values_list('name', 'city')
+    # it return Tuple
+    students = Student.objects.values_list('name', 'city', named=True)
+    # return named tuple
+    # <QuerySet [('roman', 'kathmandu'), ('razz', 'kathmandu'), ('Jack', 'Mumbai')]>
+
+    # Using: if you use multiple database and want to specify from which database you want to query in that case we can use 'using'
+    # the name that we specify on the 'setting.py' file is the name of database that we will specify here
+    """ 
+        DATABASES = {
+            'default': {
+                'ENGINE': 'django.db.backends.sqlite3',
+                'NAME': BASE_DIR / 'db.sqlite3',
+            }
+        }
+    """
+    students = Student.objects.using('default')
+    students = Student.objects.using('default').all()
+
+    # Dates:
+    students = Student.objects.dates('pass_date', 'month')
+    print(students)
+
     return render(request, 'school/home.html', {'students': students})
