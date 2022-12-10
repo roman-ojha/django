2b43174071338d4b538437ae8f5f887d917e0b3a
Roman-Ojha <razzroman98@gmail.com>
2022-12-10 17:04:39 +0545

    build: Init project for QuerySet API Aggregation function

---
+++ Tutorial/Main_Tutorial/topic_wise/71_Aggregation/school/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+class Student(models.Model):
+    name = models.CharField(max_length=70)
+    roll = models.IntegerField(unique=True, null=False)
+    city = models.CharField(max_length=70)
+    marks = models.IntegerField()
+    pass_date = models.DateField()
+    admdatetime = models.DateTimeField()
+
+
+class Teacher(models.Model):
+    name = models.CharField(max_length=70)
+    empnum = models.IntegerField(unique=True, null=False)
+    city = models.CharField(max_length=70)
+    salary = models.IntegerField()
+    join_date = models.DateField()
