6871df6742789f804124ec287ca2f75be983c11e
Roman-Ojha <razzroman98@gmail.com>
2022-12-13 21:10:45 +0545

    feat: created Models using Multi table Inheritance

--- Tutorial/Main_Tutorial/topic_wise/74_Model_Inheritance/school/admin.py
+++ Tutorial/Main_Tutorial/topic_wise/74_Model_Inheritance/school/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import ExamCenter, Student2
 
 # Register your models here.
+
+
+@admin.register(ExamCenter)
+class ExamCenterAdmin(admin.ModelAdmin):
+    list_display = ['id', 'cname', 'city']
+
+
+@admin.register(Student2)
+class Student2Admin(admin.ModelAdmin):
+    list_display = ['id', 'cname', 'city', 'name', 'roll']
+    # we know that this model table don't have field 'id', 'cname', 'city'
+    # but also we can access these field inside this model because we have inherited from 'ExamCenter' model
+    # and if you will try to insert data into 'Student2' it will also insert new data into 'ExamCenter'
--- Tutorial/Main_Tutorial/topic_wise/74_Model_Inheritance/school/models.py
+++ Tutorial/Main_Tutorial/topic_wise/74_Model_Inheritance/school/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 
+# Abstract base class Inheritance: ========================================
 # Creating Base class model
 # here for this class we don't want to create a table on database because of that we will create this class as abstract base class
 class CommonInfo(models.Model):
@@ -37,3 +38,23 @@ class Contractor(CommonInfo):
     # overriding 'date' field from base class
     date = models.DateTimeField()
     salary = models.IntegerField()
+
+
+# Multi table Inheritance: ========================================
+# https://youtu.be/cg9pb6V7IQk?list=PLbGui_ZYuhigchy8DTw4pX4duTTpvqlh6&t=2541
+class ExamCenter(models.Model):
+    cname = models.CharField(max_length=70)
+    city = models.CharField(max_length=70)
+    # field for this table are 'id', 'cname', 'city'
+
+
+class Student2(ExamCenter):
+    # here if we inherit from Base class then it will create the one to one relation with the base class
+    # means inside Student2 Table it will automatically create new field which will make relation with the base class
+    # in this case it will create a field called 'examcenter_ptr_id' inside this Model table
+    name = models.CharField(max_length=70)
+    roll = models.IntegerField()
+
+    # field for this table are 'examcenter_ptr_id', 'name', 'roll'
+
+#  Inheritance: ========================================
