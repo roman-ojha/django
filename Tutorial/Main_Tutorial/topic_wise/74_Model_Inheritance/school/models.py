b50abe0f33c61f05797f7cbb073d38d4ba6ac8f0
Roman-Ojha <razzroman98@gmail.com>
2022-12-13 20:41:03 +0545

    feat: created Models using Abstract base class Model Inheritance

--- Tutorial/Main_Tutorial/topic_wise/74_Model_Inheritance/school/models.py
+++ Tutorial/Main_Tutorial/topic_wise/74_Model_Inheritance/school/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+
+# Creating Base class model
+# here for this class we don't want to create a table on database because of that we will create this class as abstract base class
+class CommonInfo(models.Model):
+    # adding common filed that we might use on child class
+    name = models.CharField(max_length=70)
+    age = models.IntegerField()
+    date = models.DateField()
+
+    # creating this class as abstract base class
+    class Meta:
+        abstract = True
+
+
+class Student(CommonInfo):
+    # name = models.CharField(max_length=70)
+    # age = models.IntegerField()
+    fees = models.IntegerField()
+
+    # we don't want 'date' filed in the 'Student' Model/Table in that case we will assign None
+    date = None
+
+
+class Teacher(CommonInfo):
+    # name = models.CharField(max_length=70)
+    # age = models.IntegerField()
+    # date = models.DateField()
+    salary = models.IntegerField()
+
+
+class Contractor(CommonInfo):
+    # name = models.CharField(max_length=70)
+    # age = models.IntegerField()
+
+    # overriding 'date' field from base class
+    date = models.DateTimeField()
+    salary = models.IntegerField()
