526d311285e54c761fd5758c918e6ca2ce36efe6
Roman-Ojha <razzroman98@gmail.com>
2022-12-18 11:02:28 +0545

    feat(model_manager): created student model and registerd it

--- Tutorial/Main_Tutorial/topic_wise/75_Model_Manager/school/models.py
+++ Tutorial/Main_Tutorial/topic_wise/75_Model_Manager/school/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Student(models.Model):
+    name = models.CharField(max_length=70)
+    roll = models.IntegerField()
+
+    # you can change the manager name by default it is 'objects'
+    objects = models.Manager()
