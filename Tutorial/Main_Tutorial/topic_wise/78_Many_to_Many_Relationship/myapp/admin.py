5c62283ec0eea42a54683fc044189ed1d52bf9d3
Roman-Ojha <razzroman98@gmail.com>
2022-12-19 09:20:34 +0545

    feat: added Many to Many relationship between User & Song

--- Tutorial/Main_Tutorial/topic_wise/78_Many_to_Many_Relationship/myapp/admin.py
+++ Tutorial/Main_Tutorial/topic_wise/78_Many_to_Many_Relationship/myapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Song
 
-# Register your models here.
+
+@admin.register(Song)
+class SongAdmin(admin.ModelAdmin):
+    list_display = ['id', 'song_name', 'song_duration', 'sing_by']
--- Tutorial/Main_Tutorial/topic_wise/78_Many_to_Many_Relationship/myapp/models.py
+++ Tutorial/Main_Tutorial/topic_wise/78_Many_to_Many_Relationship/myapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Song(models.Model):
+    # creating many to many relationship with 'User' Model which will create new table by default
+    user = models.ManyToManyField(User)
+    song_name = models.CharField(max_length=70)
+    song_duration = models.IntegerField()
+
+    # because we can't register 'user' into './admin.py' because there could be multiple user
+    #  in that case rather then use the 'user' field we will create new function which will return all the user who Sing the song
+    def sing_by(self):
+        return ",".join([str(p) for p in self.user.all()])
+        # return : "roman,razz"
