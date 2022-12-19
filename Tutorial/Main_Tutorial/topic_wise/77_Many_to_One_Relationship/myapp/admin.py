e130d89470ffe92edec72dbe691f8cbb68c3382e
Roman-Ojha <razzroman98@gmail.com>
2022-12-19 08:43:43 +0545

    feat: created Many to One relationship with User & Post

--- Tutorial/Main_Tutorial/topic_wise/77_Many_to_One_Relationship/myapp/admin.py
+++ Tutorial/Main_Tutorial/topic_wise/77_Many_to_One_Relationship/myapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'post_title',
+                    'post_cat', 'post_published_date', 'user']
--- Tutorial/Main_Tutorial/topic_wise/77_Many_to_One_Relationship/myapp/models.py
+++ Tutorial/Main_Tutorial/topic_wise/77_Many_to_One_Relationship/myapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    # creating many to one relation with 'User' Model
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # user = models.ForeignKey(User, on_delete=models.PROTECT)
+    # user = models.ForeignKey(User, on_delete=models.SET_NULL,null=True)
+    post_title = models.CharField(max_length=70)
+    post_cat = models.CharField(max_length=70)
+    post_published_date = models.DateField(max_length=70)
