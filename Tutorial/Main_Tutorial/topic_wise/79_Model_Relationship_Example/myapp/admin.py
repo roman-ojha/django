3d199b954445fe958f68d10286bf2719b1eb4e4a
Roman-Ojha <razzroman98@gmail.com>
2022-12-21 17:12:46 +0545

    feat: create Page, Post, Song model and registered for model relationship ex

--- Tutorial/Main_Tutorial/topic_wise/79_Model_Relationship_Example/myapp/admin.py
+++ Tutorial/Main_Tutorial/topic_wise/79_Model_Relationship_Example/myapp/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Page, Post, Song
 
 # Register your models here.
+
+
+@admin.register(Page)
+class PageAdmin(admin.ModelAdmin):
+    list_display = ['page_name', 'page_cat', 'page_published_date', 'user']
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['post_title', 'post_cat', 'post_published_date', 'user']
+
+
+@admin.register(Song)
+class SongAdmin(admin.ModelAdmin):
+    list_display = ['song_name', 'song_duration', 'sing_by']
--- Tutorial/Main_Tutorial/topic_wise/79_Model_Relationship_Example/myapp/models.py
+++ Tutorial/Main_Tutorial/topic_wise/79_Model_Relationship_Example/myapp/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Page(models.Model):
+    # One to One relationship
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, primary_key=True)
+    page_name = models.CharField(max_length=70)
+    page_cat = models.CharField(max_length=70)
+    page_published_date = models.DateField()
+
+
+class Post(models.Model):
+    # Many to one relationship
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post_title = models.CharField(max_length=70)
+    post_cat = models.CharField(max_length=70)
+    post_published_date = models.DateField(max_length=70)
+
+
+class Song(models.Model):
+    # Many to many relationship
+    user = models.ManyToManyField(User)
+    song_name = models.CharField(max_length=70)
+    song_duration = models.IntegerField()
+
+    def sing_by(self):
+        return ",".join([str(p) for p in self.user.all()])
