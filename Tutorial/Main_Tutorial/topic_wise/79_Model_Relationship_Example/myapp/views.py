be2c7444f2de93b27a54c8f30b162917bc358a98
Roman-Ojha <razzroman98@gmail.com>
2022-12-21 17:25:11 +0545

    feat: created Views & Template for to display data for Model relationship example

--- Tutorial/Main_Tutorial/topic_wise/79_Model_Relationship_Example/myapp/views.py
+++ Tutorial/Main_Tutorial/topic_wise/79_Model_Relationship_Example/myapp/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from .models import Page, Post, Song
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'myapp/home.html')
+
+
+def show_page_data(request):
+    page = Page.objects.all()
+    return render(request, 'myapp/page.html', {'pages': page})
+
+
+def show_post_data(request):
+    post = Post.objects.all()
+    return render(request, 'myapp/post.html', {'posts': post})
+
+
+def show_song_data(request):
+    song = Song.objects.all()
+    return render(request, 'myapp/song.html', {'songs': song})
