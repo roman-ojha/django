d8b4f3acd04b563b4234ede4d1b58c7146c7f352
Roman-Ojha <razzroman98@gmail.com>
2022-12-22 19:39:35 +0545

    feat: used TemplateView class to render template & to pass context data

--- Tutorial/Main_Tutorial/topic_wise/81_TemplateView/config/urls.py
+++ Tutorial/Main_Tutorial/topic_wise/81_TemplateView/config/urls.py
@@ -1,21 +1,18 @@
-"""config URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
+from school import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # we can use the default 'TemplateView' provided by the django to render the template
+    path('', views.TemplateView.as_view(
+        template_name='school/home.html'), name='home'),
+
+    # using View defined by us by inheriting 'TemplateView'
+    # we can also pass extra context from here that will get pass into template
+    path(
+        'home/', views.HomeTemplateView.as_view(extra_context={'course': 'Django'}), name="home"),
+    path(
+        'home2/<int:roll>', views.HomeTemplateView.as_view(extra_context={'course': 'Django'}), name="home")
 ]
--- Tutorial/Main_Tutorial/topic_wise/81_TemplateView/school/views.py
+++ Tutorial/Main_Tutorial/topic_wise/81_TemplateView/school/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
 
-# Create your views here.
+#
+
+
+# Creating 'TemplateView' class and overriding some of the method and property that we want
+class HomeTemplateView(TemplateView):
+    template_name = 'school/home.html'
+
+    # Passing context data into template
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        # Setting new value into context
+        context['name'] = "Roman"
+        context['roll'] = 25
+
+        # OR:
+        # context = {'name': 'Roman', 'roll': 25}
+        # but this will override the context and the super value that it contain
+
+        # kwargs contain data like dynamic url data etc..
+        print(kwargs)
+        # and these value will get provided to template as context as well
+
+        return context
