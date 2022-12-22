b7d362079219aa59f8a45c2d465c295443529029
Roman-Ojha <razzroman98@gmail.com>
2022-12-22 22:07:38 +0545

    feat: used RedirectView on class based view

--- Tutorial/Main_Tutorial/topic_wise/82_RedirectView/config/urls.py
+++ Tutorial/Main_Tutorial/topic_wise/82_RedirectView/config/urls.py
@@ -1,21 +1,23 @@
-"""config URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
+from school import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.TemplateView.as_view(
+        template_name='school/home.html'), name="main"),
+
+    # we will redirect to: '' path if someone will come to 'home/', 'index/' path
+    path('home/', views.RedirectView.as_view(url='/'), name='home'),
+    # redirecting using pattern_name that 'name' that we specify on every url path
+    path('index/', views.RedirectView.as_view(pattern_name="main"), name='index'),
+
+    # redirect on other url
+    path('yt/', views.RedirectView.as_view(url="https://www.youtube.com/"), name='yt'),
+    path('youtube/', views.NewRedirectView.as_view(), name='youtube'),
+
+    # route to pass dynamic url value & redirecting to '<int:pk>/' url using 'SecondNewRedirectView' which uses pattern_name
+    path('home/<int:pk>', views.SecondNewRedirectView.as_view(), name='homepk'),
+    # because we are using 'pattern_name' rather then url on 'SecondNewRedirectView' that 'pk' that get pass to that url will get redirect with the same 'pk' on '<int:pk>/' url
+    path('<int:pk>/', views.TemplateView.as_view(template_name='school/home.html'), name='mainpk')
 ]
--- Tutorial/Main_Tutorial/topic_wise/82_RedirectView/school/views.py
+++ Tutorial/Main_Tutorial/topic_wise/82_RedirectView/school/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.views.generic.base import TemplateView, RedirectView
 
-# Create your views here.
+
+# Creating a new RedirectView using 'RedirectView'
+class NewRedirectView(RedirectView):
+    url = 'https://www.youtube.com'
+
+    # pattern_name = ''
+
+
+class SecondNewRedirectView(RedirectView):
+    pattern_name = 'mainpk'
+
+    # permanent = False(default) : gives 302 status code
+    # permanent = True : gives 301 status code
+    permanent = True
+
+    query_string = True
+    # query string is accepted on this url now and will not get remove on requesting using this url
+    # default: False
+
+    def get_redirect_url(self, *args, **kwargs):
+        print(kwargs)
+        # we ca access the dynamic url value and manipulate here as well
+        kwargs['pk'] = 10
+        return super().get_redirect_url(*args, **kwargs)
