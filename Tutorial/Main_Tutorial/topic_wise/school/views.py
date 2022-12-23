82f852b9b31f2e524b16600e2d38789fa0010160
Roman-Ojha <razzroman98@gmail.com>
2022-12-23 15:58:17 +0545

    feat(ListView): created Models used ListView to render list os Student on template

--- Tutorial/Main_Tutorial/topic_wise/school/views.py
+++ Tutorial/Main_Tutorial/topic_wise/school/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from .models import Student
 
-# Create your views here.
+
+# Creating generic class view
+class StudentListView(ListView):
+    model = Student
+    # passing model while will run:
+    # Student.object.all()
+
+    # and we will get context data inside 'object_list' || 'student_list'
+    # we can change the default context name for 'student_list'
+    # but 'object_list' will get pass after changing context object name
+    context_object_name = 'students'
+
+    # default template name this view will render is 'school/student_list.html'
