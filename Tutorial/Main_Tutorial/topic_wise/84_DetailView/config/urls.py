32fd7a2759b2ae9cc48ba4e5f56971f13d744a04
Roman-Ojha <razzroman98@gmail.com>
2022-12-23 17:44:40 +0545

    feat: used DetailView from generic class based view

--- Tutorial/Main_Tutorial/topic_wise/84_DetailView/config/urls.py
+++ Tutorial/Main_Tutorial/topic_wise/84_DetailView/config/urls.py
@@ -1,21 +1,12 @@
-"""config URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
+from school import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # Passing pk dynamic value from url to 'DetailView' class
+    path('student/<int:id>', views.StudentDetailView.as_view(), name='student'),
+
+    path('student/', views.StudentListView.as_view(), name='students')
 ]
--- Tutorial/Main_Tutorial/topic_wise/84_DetailView/school/views.py
+++ Tutorial/Main_Tutorial/topic_wise/84_DetailView/school/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
+from .models import Student
+from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
+
 
 # Create your views here.
+class StudentDetailView(DetailView):
+    # Providing the 'Student' model for this view
+    model = Student
+    # Because this return the object detail rather then List
+    # it means that while running this view from the route it expect 'pk'
+
+    # Default template name for this model 'student_detail.html'
+    # Setting custom template name
+    template_name = 'school/student.html'
+    # Now default template name will now work
+
+    # Default context for this model is 'student'
+    # Setting custom context object name
+    context_object_name = 'st'
+
+    # to change the default 'pk' kwargs to query 'Student' Detail
+    pk_url_kwarg = 'id'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+
+        # passing all remaining student so that we can navigate to the specific student page from other specific student page
+        context['remaining_student'] = self.model.objects.exclude(
+            id=context['object'].id)
+        return context
+
+
+class StudentListView(ListView):
+    model = Student
