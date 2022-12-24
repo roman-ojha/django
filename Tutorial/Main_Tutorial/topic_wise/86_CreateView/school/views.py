b25cc5383edca29ad96d91a1d3c28ec7eff37941
Roman-Ojha <razzroman98@gmail.com>
2022-12-24 17:09:09 +0545

    feat: created models, CreateView, and rendered it to template

--- Tutorial/Main_Tutorial/topic_wise/86_CreateView/school/views.py
+++ Tutorial/Main_Tutorial/topic_wise/86_CreateView/school/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
+from .models import Student
+
 
 # Create your views here.
+class CreateStudentView(CreateView):
+    # passing the mode where we want to save the valid form that we get from user
+    model = Student
+    # now the field that we provide these are the field from which it will create a new form and send it into the template to get render
+    fields = ['name', 'email', 'password']
