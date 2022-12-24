e8a404528d22a574b2860dd5ff1a5f855f354108
Roman-Ojha <razzroman98@gmail.com>
2022-12-24 16:14:57 +0545

    feat: created form, FormView & render form into template

--- Tutorial/Main_Tutorial/topic_wise/85_FormView/school/views.py
+++ Tutorial/Main_Tutorial/topic_wise/85_FormView/school/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .forms import ContactForm
+from django.views.generic.edit import FormView
 
 # Create your views here.
+
+# using FormView class to render and validate form
+
+
+class ContactFormView(FormView):
+    # we will pass the form into the given template
+    template_name = 'school/contact.html'
+    # we will use the Form class to create a new form and it will automatically pass that form into the given template
+    form_class = ContactForm
+
+    # After successfully validate the form data when user submit we will redirect to the given url
+    success_url = '/thankyou/'
