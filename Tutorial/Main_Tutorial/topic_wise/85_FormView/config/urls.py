15ca3a46d7cc22146d611c53085dc13c4d03c7de
Roman-Ojha <razzroman98@gmail.com>
2022-12-24 16:37:34 +0545

    feat: used FormView to render, validated and get form data

--- Tutorial/Main_Tutorial/topic_wise/85_FormView/config/urls.py
+++ Tutorial/Main_Tutorial/topic_wise/85_FormView/config/urls.py
@@ -5,4 +5,5 @@ from school import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('contact/', views.ContactFormView.as_view(), name='contact'),
+    path('thankyou/', views.ThanksTemplateView.as_view(), name='thankyou'),
 ]
--- Tutorial/Main_Tutorial/topic_wise/85_FormView/school/views.py
+++ Tutorial/Main_Tutorial/topic_wise/85_FormView/school/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render
 from .forms import ContactForm
 from django.views.generic.edit import FormView
+from django.views.generic.base import TemplateView
+from django.http import HttpResponse
 
-# Create your views here.
 
 # using FormView class to render and validate form
-
-
 class ContactFormView(FormView):
     # we will pass the form into the given template
     template_name = 'school/contact.html'
@@ -15,3 +14,25 @@ class ContactFormView(FormView):
 
     # After successfully validate the form data when user submit we will redirect to the given url
     success_url = '/thankyou/'
+
+    # passing the initial data when form is getting rendered
+    initial = {'name': 'Roman', 'email': 'roman@gmail.com'}
+
+    def form_valid(self, form):
+        # we can get the form data after it get validate in this function
+        form_data = form.cleaned_data
+        print(form_data)
+
+        # on this return it will redirect to 'success_url'
+        return super().form_valid(form)
+
+        # on this return it will ont redirect and response Http rather
+        # return HttpResponse("Form submitted")
+
+    def form_invalid(self, form):
+        return super().form_invalid(form)
+
+
+# redirect to this view after form got validate and get success
+class ThanksTemplateView(TemplateView):
+    template_name = 'school/thankyou.html'
