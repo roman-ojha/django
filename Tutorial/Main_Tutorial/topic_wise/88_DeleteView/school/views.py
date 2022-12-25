09f43b9c0f187eb97956f6595443a176108b2ac6
Roman-Ojha <razzroman98@gmail.com>
2022-12-25 12:47:59 +0545

    feat: used DeleteView to delete model object

--- Tutorial/Main_Tutorial/topic_wise/88_DeleteView/school/views.py
+++ Tutorial/Main_Tutorial/topic_wise/88_DeleteView/school/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.views.generic.edit import UpdateView, CreateView, DeleteView
+from django.views.generic.base import TemplateView
+from .models import Student
+from django import forms
 
-# Create your views here.
+
+class CreateStudentView(CreateView):
+    model = Student
+    fields = ['name', 'email', 'password']
+    success_url = '/thanks/'
+
+
+class ThanksTemplateView(TemplateView):
+    template_name = 'school/thanks.html'
+
+
+class UpdateStudentView(UpdateView):
+    model = Student
+    fields = ['name', 'email', 'password']
+
+    success_url = '/thanks/'
+
+    def get_form(self):
+        form = super().get_form()
+        form.fields['name'].widget = forms.TextInput(
+            attrs={'class': 'name-field'})
+        form.fields['password'].widget = forms.PasswordInput(
+            render_value=True, attrs={'class': 'password-field'})
+        return form
+
+
+# Delete View to delete model object
+class DeleteStudentView(DeleteView):
+    # providing model from where we want to delete
+    model = Student
+
+    # Success url after object get deleted
+    success_url = '/create/'
+
+    # Before deleting the object it will first redirect to the confirmation page
+    # By default the template for confirmation page is '<model>_confirm_delete.html'
