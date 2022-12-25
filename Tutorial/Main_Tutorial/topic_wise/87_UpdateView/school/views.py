de29d48cb488485a05e8b95b1cf81f2b6f8749d5
Roman-Ojha <razzroman98@gmail.com>
2022-12-25 12:09:40 +0545

    feat: created models, and create obj & update view using UpdateView

--- Tutorial/Main_Tutorial/topic_wise/87_UpdateView/school/views.py
+++ Tutorial/Main_Tutorial/topic_wise/87_UpdateView/school/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.views.generic.edit import UpdateView, CreateView
+from django.views.generic.base import TemplateView
+from .models import Student
 
-# Create your views here.
+# We will create a view for Creating Object as well as Update Object
+
+
+# View to create new 'Student' object
+class CreateStudentView(CreateView):
+    model = Student
+    fields = ['name', 'email', 'password']
+    success_url = '/thanks/'
+
+
+class ThanksTemplateView(TemplateView):
+    template_name = 'school/thanks.html'
+
+# View to update 'Student' object
+
+
+class UpdateStudentView(UpdateView):
+    # here we will specify on which model you want to update data
+    model = Student
+    # in which field you want to update data
+    fields = ['name']
