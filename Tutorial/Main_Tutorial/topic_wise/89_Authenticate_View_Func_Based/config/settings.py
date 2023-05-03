913b589ee8f82783277d2cbe0a3bcccdc9ba8452
Roman-Ojha <razzroman98@gmail.com>
2023-05-03 21:43:23 +0545

    feat: complated authentication views with function based

--- Tutorial/Main_Tutorial/topic_wise/89_Authenticate_View_Func_Based/config/settings.py
+++ Tutorial/Main_Tutorial/topic_wise/89_Authenticate_View_Func_Based/config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'registration',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = "/accounts"
+# OR
+LOGIN_URL = "/accounts/login/"
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
