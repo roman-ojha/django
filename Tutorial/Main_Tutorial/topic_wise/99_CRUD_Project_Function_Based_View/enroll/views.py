affc2c21e06573a8b30e83f735265b0845f227d5
Roman-Ojha <razzroman98@gmail.com>
2023-05-04 20:11:08 +0545

    feat: showing & adding student data

--- Tutorial/Main_Tutorial/topic_wise/99_CRUD_Project_Function_Based_View/enroll/views.py
+++ Tutorial/Main_Tutorial/topic_wise/99_CRUD_Project_Function_Based_View/enroll/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
+from .forms import StudentRegistration
+from .models import User
 
 
 def addAndShow(request):
-    return render(request, 'enroll/addandshow.html')
+    if request.method == "POST":
+        fm = StudentRegistration(request.POST)
+        if fm.is_valid():
+            # fm.save()
+            name = fm.cleaned_data['name']
+            email = fm.cleaned_data['email']
+            password = fm.cleaned_data['password']
+            user = User(name=name, email=email, password=password)
+            user.save()
+        fm = StudentRegistration()
+        students = User.objects.all()
+    else:
+        fm = StudentRegistration()
+        students = User.objects.all()
+    return render(request, 'enroll/addandshow.html', {'form': fm, 'students': students})
