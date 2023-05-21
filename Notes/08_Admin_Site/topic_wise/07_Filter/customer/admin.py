5ac2d762d301c85f7278bfc9b9ddb7bd63ce0153
Roman-Ojha <razzroman98@gmail.com>
2023-05-21 08:45:56 +0545

    feat(admin filter): added custom BusinessEmailFilter

--- Notes/08_Admin_Site/topic_wise/07_Filter/customer/admin.py
+++ Notes/08_Admin_Site/topic_wise/07_Filter/customer/admin.py
@@ -1,3 +1,50 @@
+from .models import Profile
+from django.contrib.admin import SimpleListFilter
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+# Creating the Custom filter:
+# This filter is being used in django admin panel in profile model.
+class BusinessEmailFilter(SimpleListFilter):
+    # title/name of the filter that get displayed in Django admin filter sidebar
+    title = 'Email Types'
+    # so here we are selecting "User" model of "email" fields
+    parameter_name = 'user__email'  # '<model>__<field>'
+
+    def lookups(self, request, model_admin):
+        # So here we will define what are the choices or options that we can use to filter
+        return (
+            # So here we are categorizing our email with 'business' & 'non-business' email
+            # ('<django_use_as_value>','<human_readable_shown_in_admin>')
+            ('business', 'Business'),
+            ('non_business', 'Non Business')
+        )
+
+    # So here we created the REGEX so that we can filter using this, here we are validating about if the email is from 'hotmail', 'gmail', 'yahoo' then it is a non-business email
+    SOCIAL_EMAIL_REGEX = r"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(?!hotmail|gmail|yahoo)(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$"
+
+    def queryset(self, request, queryset):
+        # So filter is just the query so now we will build our one query which will then filter the record
+        if not self.value():  # if we haven't select any options then return all the queryset
+            return queryset
+        if self.value().lower() == 'business':
+            # now here we are filtering based on the business email
+            return queryset.filter(user__email__regex=self.SOCIAL_EMAIL_REGEX)
+        elif self.value().lower() == 'non_business':  # same goes not this filter
+            return queryset.filter().exclude(user__email__regex=self.SOCIAL_EMAIL_REGEX)
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ("id", "get_email", "full_name",
+                    "created_at", "role", "is_verified")
+    # We are filtering with some fields
+    # Added Custom "BusinessEmailFilter" filter
+    list_filter = ("created_at", "role", "is_verified", BusinessEmailFilter)
+
+    # By default when we will select some filters for the Date & time fields in that case django will search from greater then date to upto less then date
+    # So, why don't we create the date picker from start date to the end date upto given date then it will going to filter the records
+
+
+admin.site.register(Order)
--- Notes/08_Admin_Site/topic_wise/07_Filter/customer/models.py
+++ Notes/08_Admin_Site/topic_wise/07_Filter/customer/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    ROLE_CHOICES = (
+        (1, 'Customer'),
+        (2, 'Vendor'),
+        (3, 'Admin'),
+    )
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    location = models.CharField(max_length=30, blank=True)
+    birthdate = models.DateField(null=True, blank=True)
+    role = models.PositiveSmallIntegerField(
+        choices=ROLE_CHOICES, null=True, blank=True)
+    is_verified = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    @property
+    def full_name(self):
+        return "%s %s" % (self.user.first_name, self.user.last_name)
+
+    @property
+    def get_email(self):
+        return "%s" % (self.user.email)
+
+    def __str__(self):
+        return self.user.username
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    placed_at = models.DateTimeField(auto_now_add=True)
