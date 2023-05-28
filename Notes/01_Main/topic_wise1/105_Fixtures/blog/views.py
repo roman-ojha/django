645671df7755e3d92818b3de991da5059d8d8700
Roman-Ojha <razzroman98@gmail.com>
2023-05-28 18:16:39 +0545

    feat(fixture): added simple BlogPost model & rendered it's data on template

--- Notes/01_Main/topic_wise1/105_Fixtures/blog/views.py
+++ Notes/01_Main/topic_wise1/105_Fixtures/blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import BlogPost
 
 # Create your views here.
+
+
+def home(request):
+    context = {
+        'post': BlogPost.objects.all()
+    }
+    return render(request, 'home.html', context)
